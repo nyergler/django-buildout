3ef94f3bd878844d6b7e373f1a208bb3985dea2f
nathanyergler <nathanyergler@c5640ed2-ea24-0410-93c5-9ba48f47c51d>
2006-12-19 19:00:28 +0000

    Generate django-admin and manage scripts.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "django_buildout",
-    version = "0.2",
+    version = "0.3",
     packages = find_packages('src'),
     package_dir = {'':'src'},
 
@@ -12,13 +12,18 @@ setup(
     install_requires = ['setuptools',
                         'zc.buildout',
                         'zc.recipe.egg',
+                        'Django',
                         ],
     include_package_data = True,
     zip_safe = True,
 
     # entry point for zc.buildout recipe
     entry_points = {
-       'zc.buildout':['deploy = django_buildout.deploy:DjangoDeploy',],
+       'zc.buildout':
+           ['deploy = django_buildout.deploy:DjangoDeploy',],
+       'console_scripts':
+           ['django-admin = django_buildout.scripts:djangoAdmin',
+            'manage = django_buildout.scripts:projectManage',],
     },
     
     # author metadata
